0b0c2f2855ac9db792486175d7b349e8914d9bf9
Marek Skrzelowski <marek.skrzelowski@vandermesis.com>
2023-09-01 00:16:53 +0200

    Add AddPanelModifier view modifier

---
+++ Notki/AddPanelModifier.swift
@@ -0,0 +1,46 @@
+//
+//  AddPanelModifier.swift
+//  Notki
+//
+//  Created by Marek Skrzelowski on 31/08/2023.
+//
+
+import SwiftUI
+
+fileprivate struct AddPanelModifier<PanelContent: View>: ViewModifier {
+    @State var panel: AddPanel<PanelContent>?
+    @Binding var isPresented: Bool
+    @ViewBuilder let view: () -> PanelContent
+    var contentRect: CGRect = CGRect(x: 0, y: 0, width: 624, height: 512)
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                panel = AddPanel(view: view, contentRect: contentRect, isPresented: $isPresented)
+                panel?.center()
+                if isPresented { present() }
+            }
+            .onDisappear {
+                panel?.close()
+                panel = nil
+            }
+            .onChange(of: isPresented) { value in
+                value ? present() : panel?.close()
+            }
+    }
+
+    func present() {
+        panel?.orderFront(nil)
+        panel?.makeKey()
+    }
+}
+
+extension View {
+    func floatingPanel<Content: View>(
+        isPresented: Binding<Bool>,
+        contentRect: CGRect = CGRect(x: 0, y: 0, width: 624, height: 512),
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View {
+        self.modifier(AddPanelModifier(isPresented: isPresented, view: content, contentRect: contentRect))
+    }
+}
