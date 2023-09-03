3e72d9324dc1a377e74e9d4511413beb9d7a390b
Marek Skrzelowski <marek.skrzelowski@vandermesis.com>
2023-09-03 22:40:46 +0200

    Test of presenting FloatingPanel

--- Notki/NewNoteView.swift
+++ Notki/NewNoteView.swift
@@ -7,26 +7,21 @@
 
 import SwiftUI
 
-@available(macOS 14.0, *)
 struct NewNoteView: View {
-    @State private var input: String = ""
-
+    @State var showingPanel = false
+    
     var body: some View {
-        VStack(spacing: 20) {
-            TextField("Szukaj...", text: $input)
-                .font(.title)
-                .padding()
-                .background(Color(.systemFill))
-                .cornerRadius(10)
-                .shadow(radius: 5)
-
-            List {
-                // Przykładowe pozycje do wyświetlenia:
-                Text("Pozycja 1")
-                Text("Pozycja 2")
-                Text("Pozycja 3")
+        Button("Present panel") {
+            showingPanel.toggle()
+        }
+        .floatingPanel(isPresented: $showingPanel) {
+            ZStack {
+                Rectangle()
+                    .fill(.white)
+                Text("I'm floating panel. Click anywhere to dismiss me.")
+                    .font(.title)
+                    .foregroundStyle(.blue)
             }
         }
-        .padding()
     }
 }
